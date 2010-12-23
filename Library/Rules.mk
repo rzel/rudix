169dae99ce6da8f7a1bfb7edaba700f0c6359176
'Rud? Moura <ruda.moura@gmail.com>
2010-12-23 16:19:17 -0200

    Adopted Node.JS Initial packaging for i386 or x86_64 build.

---
+++ Library/Rules.mk
@@ -0,0 +1,131 @@
+# -*- mode: makefile -*-
+#
+# Common Rules and Macros
+# Copyright (c) 2005-2010 Ruda Moura <ruda@rudix.org>
+#
+
+BUILDSYSTEM=	20101115
+
+VENDOR=		org.rudix
+PORTDIR:=	$(shell pwd)
+BUILDDIR=	$(PORTDIR)/$(NAME)-$(VERSION)
+INSTALLDIR=	$(PORTDIR)/$(NAME)-install
+INSTALLDOCDIR=	$(INSTALLDIR)/usr/local/share/doc/$(NAME)
+PKGNAME=	$(PORTDIR)/$(NAME).pkg
+DMGNAME=	$(PORTDIR)/$(NAME)-$(VERSION)-$(REVISION).dmg
+TITLE=		$(NAME) $(VERSION)
+
+PACKAGEMAKER=	/Developer/usr/bin/packagemaker
+CREATEDMG=	/usr/bin/hdiutil create
+TOUCH=		touch
+#TOUCH=		@date >
+FETCH=		curl -f -O -C - -L
+#FETCH=		wget -c
+
+# Detect architecture (Intel or PowerPC) and number of CPUs/Cores
+ARCH:=		$(shell arch)
+NCPU:=		$(shell sysctl -n hw.ncpu)
+CPU64BIT:=	$(shell sysctl -n hw.cpu64bit_capable)
+
+# Build flags on Snow Leopard
+CFLAGS=		-arch i386 -arch x86_64 -Os
+CXXFLAGS=	-arch i386 -arch x86_64 -Os
+LDFLAGS=	-arch i386 -arch x86_64
+
+## Build flags on Leopard
+#CFLAGS=	-arch i386 -arch ppc -Os
+#CXXFLAGS=	-arch i386 -arch ppc -Os
+#LDFLAGS=	-arch i386 -arch ppc
+
+## Debug flags:
+#CFLAGS=	-ggdb
+#CXXFLAGS=	-ggdb
+#LDFLAGS=
+
+#
+# Build rules
+#
+all: install
+
+help:
+	@echo "make <action> where action is:"
+	@echo "  help		this help message"
+	@echo "  retrieve	retrieve files necessary to compile"
+	@echo "  prep		explode source, apply patches, etc"
+	@echo "  build		configure software and then build it"
+	@echo "  install	install software into directory $(INTALLDIR)"
+	@echo "  pkg		create a package (.pkg)"
+	@echo "  dmg		create a disk image (.dmg)"
+	@echo "  installpkg	install the package created"
+	@echo "  all		do prep, build and install"
+	@echo "  installclean	local installation clean-up"
+	@echo "  clean		build and local installation clean-up"
+	@echo "  distclean	clean-up  many things but keep sources"
+	@echo "  realdistclean	clean-up everything else"
+	@echo "make without any action does 'make all'"
+
+retrieve:
+	$(FETCH) $(URL)/$(SOURCE)
+	touch retrieve
+
+# Note: rules prep, build, install must be defined in your Makefile!
+
+pkg: install
+	$(PACKAGEMAKER) \
+		--doc $(NAME).pmdoc \
+		--id $(VENDOR).pkg.$(NAME) \
+		--version $(VERSION)-$(REVISION) \
+		--title "$(TITLE)" \
+	$(if $(wildcard $(PORTDIR)/scripts),--scripts $(PORTDIR)/scripts) \
+		--out $(PKGNAME)
+	touch pkg
+
+dmg: pkg
+	$(CREATEDMG) \
+		-volname "$(NAME)" \
+		-srcfolder $(README) \
+		-srcfolder $(LICENSE) \
+		-srcfolder $(PKGNAME) $(DMGNAME)
+	touch dmg
+
+installpkg: pkg
+	installer -pkg $(PKGNAME) -target /
+
+installclean:
+	rm -rf install $(INSTALLDIR)
+
+pkgclean:
+	rm -rf pkg *.pkg
+
+dmgclean:
+	rm -rf dmg *.dmg
+
+clean: installclean
+	rm -rf prep build test $(BUILDDIR)
+
+distclean: clean pkgclean dmgclean
+	rm -f config.cache*
+
+realdistclean: distclean
+	rm -f retrieve $(SOURCE)
+
+tag:
+	svn update
+	svn copy . https://rudix.googlecode.com/svn/tags/pool/$(NAME)/$(NAME)-$(VERSION)-$(REVISION) -m "[TAG] $(NAME) version $(VERSION) revision $(REVISION)"
+
+about:
+	@echo "$(TITLE) ($(NAME)-$(VERSION)-$(REVISION))"
+
+#
+# Handful macros
+#
+define configure
+./configure \
+	--cache-file=$(PORTDIR)/config.cache \
+	--mandir=/usr/local/share/man \
+	--infodir=/usr/local/share/info
+endef
+
+define make
+make -j $(NCPU)
+endef
---
+++ Ports/rudix/rudix.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+
+'''Rudix Package Manager -- RPM ;D
+
+Usage: rudix [-h|-v|-l|-R|-K|-u] [-I <pkg-path>|-L <package-id>|-i <package-id>|-r <package-id>|-S <path>|-V <package-id>|-f <package-id>|-n <package-id>]
+List all installed packages (package-id) unless options are given, like:
+  -h    This help message
+  -v    Print version
+  -l    List all installed packages (package-id, version and install date)
+  -I    Print package information (package-id, version and install date)
+  -L    List package content
+  -i    Install package (download if not a file)
+  -r    Remove package
+  -R    Remove *all* Rudix packages installed (ask to confirm)
+  -S    Search for <path> in all packages and print if matched
+  -V    Verify package
+  -K    Verify all installed packages
+  -f    Fix (repair) package
+  -n    Download and install package (remote install)
+  -u    Download and install all updated packages (remote update)
+
+Where <package-id> is either org.rudix.pkg.<name> or <name>.
+'''
+
+import sys
+import os
+import getopt
+import tempfile
+from subprocess import Popen, PIPE, call
+
+__author__ = 'Ruda Moura'
+__copyright__ = 'Copyright (c) 2005-2010 Ruda Moura <ruda@rudix.org>'
+__credits__ = 'Ruda Moura, Leonardo Santagada'
+__license__ = 'BSD'
+__version__ = '@VERSION@'
+
+PROG_NAME = os.path.basename(sys.argv[0])
+PREFIX = 'org.rudix.pkg.'
+
+def rudix_version():
+    'Print version and exit'
+    print 'Rudix Package Manager version %s' % __version__
+    print __copyright__
+    sys.exit(0)
+
+def usage():
+    'Print help and exit'
+    print __doc__
+    sys.exit(0)
+
+def root_required():
+    'Check for root and pass or exit if not'
+    if os.getuid() != 0:
+        print >> sys.stderr, '%s: this operation requires root privileges'%PROG_NAME
+        sys.exit(1)
+
+def communicate(args):
+    'Call a process and return its stdout data as a list of strings'
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    return proc.communicate()[0].split('\n')[:-1]
+
+def get_packages():
+    'Get a list of packages installed'
+    out = communicate(['pkgutil', '--pkgs=' + PREFIX + '*'])
+    pkgs = [line.strip() for line in out]
+    return pkgs
+
+def get_package_info(pkg):
+    'Get information from pkg'
+    out = communicate(['pkgutil', '-v', '--pkg-info', pkg])
+    version = None
+    install_date = None
+    for line in out:
+        line = line.strip()
+        if line.startswith('version: '):
+            version = line[len('version: '):]
+        elif line.startswith('install-time: '):
+            install_date = line[len('install-time: '):]
+    return version, install_date
+
+def get_package_content(pkg):
+    'Get a list of file names from pkg'
+    out = communicate(['pkgutil', '--files', pkg, '--only-files'])
+    content = ['/'+line.strip() for line in out]
+    return content
+
+def print_package_info(pkg):
+    'Print information about pkg'
+    version, install_date = get_package_info(pkg)
+    if install_date is not None:
+        print '%s version %s (install: %s)'%(pkg, version, install_date)
+    else:
+        print "No receipt for '%s' found at '/'."%pkg # pretend we are pkgutil
+
+def list_all_packages():
+    'List all packages installed'
+    for pkg in get_packages():
+        print pkg
+
+def list_all_packages_info():
+    'List all packages installed, more detailed'
+    for pkg in get_packages():
+        print_package_info(pkg)
+
+def list_package_files(pkg):
+    'Print the file names of pkg'
+    content = get_package_content(pkg)
+    for file in content:
+        print file
+
+def install_package(pkg):
+    'Install a local pkg'
+    root_required()
+    call(['installer', '-pkg', pkg, '-target', '/'], stderr=PIPE)
+
+def remove_package(pkg):
+    'Uninstall a pkg'
+    root_required()
+    devnull = open('/dev/null')
+    call(['pkgutil', '--unlink', pkg, '-f'], stderr=devnull)
+    call(['pkgutil', '--forget', pkg], stderr=devnull)
+    devnull.close()
+
+def remove_all_packages():
+    'Uninstall all packages'
+    root_required()
+    print "Using this option will remove *all* Rudix's packages!"
+    print "Are you sure you want to proceed? (answer 'yes' or 'y' to confirm)"
+    answer = raw_input().strip()
+    if answer not in ['yes', 'y']:
+        print 'Good!'
+        return
+    print 'Removing packages...'
+    for pkg in get_packages():
+        print pkg
+        remove_package(pkg)
+    # remember LinuxConf...
+    print 'Cry a little tear because Rudix was removed'
+
+def search_in_packages(path):
+    'Search for path in all packages'
+    out = communicate(['pkgutil', '--file-info', path])
+    for line in out:
+        line = line.strip()
+        if line.startswith('pkgid: '):
+            print line[len('pkgid: '):]
+
+def verify_package(pkg):
+    'Verify pkg sanity'
+    call(['pkgutil', '--verify', pkg], stderr=PIPE)
+
+def verify_all_packages():
+    'Verify all packages sanity'
+    for pkg in get_packages():
+        verify_package(pkg)
+
+def fix_package(pkg):
+    'Try to fix permissions and groups of pkg'
+    root_required()
+    call(['pkgutil', '--repair', pkg], stderr=PIPE)
+
+def version_compare(v1, v2):
+    from distutils.version import LooseVersion
+    # remove the release if it is 0
+    if v1.endswith('-0'): v1 = v1[:-2]
+    if v2.endswith('-0'): v2 = v2[:-2]
+    return cmp(LooseVersion(v1), LooseVersion(v2))
+
+def find_net_info(pkg):
+    'Search the download page for versions of pkg'
+    from urllib2 import urlopen
+    import re
+    pkg = denormalize(pkg)
+    cont = urlopen('http://code.google.com/p/rudix/downloads/list?q=%s'%pkg).read()
+    urls = re.findall('(http://rudix.googlecode.com/files/(%s-([0-9.]+(?:-[0-9]+)?(?:.i386)?)\.dmg))'%pkg, cont)
+    versions = sorted(list(set(urls)), cmp=lambda x,y: version_compare(x[1],y[1]))
+    if len(versions) == 0:
+        return None
+    else:
+        return versions[-1]
+
+def net_install_package(pkg, net_info):
+    'Support function for net_install_command'
+    root_required()
+    net_url, net_filename, net_version = net_info
+    print 'Downloading', net_url
+    tempf, file_path = tempfile.mkstemp()
+    try:
+        call(['curl', '-f', '-o', file_path, '-C', '-', '-L', '-#', net_url])
+        print 'Mounting downloaded image file', file_path
+        out = communicate(['hdiutil', 'attach', file_path])
+        for l in out:
+            if 'Apple_partition_scheme' in l:
+                disk_path = l.split()[0]
+            if 'Apple_HFS' in l:
+                volume_path = l.split()[2]
+
+        install_package(os.path.join(volume_path, denormalize(pkg)+'.pkg'))
+        print 'Unmounting image', volume_path
+        call(['hdiutil', 'detach', disk_path], stdout=PIPE, stderr=PIPE)
+    finally:
+        os.close(tempf)
+
+def net_install_command(pkg):
+    'Install a pkg from the internet if the pkg was not installed or is older than the internet version'
+    net_info = find_net_info(pkg)
+    version, install_date = get_package_info(pkg)
+    if net_info is None:
+        print "Package '%s' not found online"%pkg
+        return
+    if version is not None and version_compare(version, net_info[2]) >= 0:
+        print 'Latest version of package %s(%s) already installed'%(pkg, version)
+        return
+    net_install_package(pkg, net_info)
+    print 'All done'
+
+def update_all_packages():
+    'Try to update the current base of packages'
+    to_update = []
+    # take each package, go to the internet and see if there is a newer version
+    for pkg in get_packages():
+        net_info = find_net_info(pkg)
+        version, install_date = get_package_info(pkg)
+        if net_info is None or version_compare(version, net_info[2]) >= 0:
+            continue
+        print '{0:25} {1:10} will be updated to version {2}'.format(denormalize(pkg), version, net_info[2])
+        to_update.append((pkg, net_info))
+    if len(to_update) == 0:
+        print 'All packages are up to date'
+        return
+    # if there is packages to update you need to be root
+    root_required()
+    for pkg, net_info in to_update:
+        net_install_package(pkg, net_info)
+    print 'All done'
+
+def normalize(pkg):
+    'Transform pkg in full pkg-id (with PREFIX)'
+    if not pkg.startswith(PREFIX):
+        pkg = PREFIX + pkg
+    return pkg
+
+def denormalize(pkg):
+    'Transform pkg in name without PREFIX'
+    if pkg.startswith(PREFIX):
+        pkg = pkg[len(PREFIX):]
+    return pkg
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    if len(argv) == 1:
+        list_all_packages()
+        sys.exit(0)
+    try:
+        opts, args = getopt.getopt(argv[1:], "hI:lL:i:r:RS:vV:Kf:n:u")
+    except getopt.error, msg:
+        print >> sys.stderr, '%s: %s'%(PROG_NAME, msg)
+        print >> sys.stderr, '\t for help use -h'
+        sys.exit(2)
+    # option processing
+    for option, value in opts:
+        if option == '-h':
+            usage()
+        if option == '-v':
+            rudix_version()
+        if option == '-I':
+            print_package_info(normalize(value))
+        if option == '-l':
+            list_all_packages_info()
+        if option == '-L':
+            list_package_files(normalize(value))
+        if option == '-i':
+            if os.path.isfile(value):
+                install_package(value)
+            else:
+                net_install_command(normalize(value))
+        if option == '-r':
+            remove_package(normalize(value))
+        if option == '-R':
+            remove_all_packages()
+        if option == '-S':
+            search_in_packages(value)
+        if option == '-V':
+            verify_package(normalize(value))
+        if option == '-K':
+            verify_all_packages()
+        if option == '-f':
+            fix_package(normalize(value))
+        if option == '-n':
+            net_install_command(normalize(value))
+        if option == '-u':
+            update_all_packages()
+
+if __name__ == "__main__":
+    main()

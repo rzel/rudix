f4b619aab45cb1995b373161a12268ad2ecd4210
'Rud? Moura <ruda.moura@gmail.com>
2011-04-30 15:51:42 -0300

    pip: updated to the new buildsystem. Upgraded to version 1.0.

---
+++ Library/GNUFormula.mk
@@ -0,0 +1,47 @@
+# GNUFormula.mk - Formula to build in GNU Style (with help of configure)
+# Copyright (c) 2011 Ruda Moura
+# Authors: Ruda Moura, Leonardo Santagada
+
+ifeq ($(RUDIX_ENABLE_NLS),yes)
+GnuConfigureExtra += --enable-nls
+BuildRequires += /usr/local/lib/libintl.la
+else ifeq ($(RUDIX_ENABLE_NLS),no)
+GnuConfigureExtra += --disable-nls
+endif
+
+define build_inner_hook
+$(call info_color,Running Configure)
+cd $(BuildDir) ; \
+env CFLAGS="$(CFlags)" CXXFLAGS="$(CxxFlags)" LDFLAGS="$(LdFlags)" $(EnvExtra) \
+$(gnu_configure)
+$(call info_color,Done)
+cd $(BuildDir) ; $(gnu_make) $(GnuMakeExtra)
+endef
+
+define install_inner_hook
+cd $(BuildDir) ; \
+$(gnu_make) install DESTDIR="$(PortDir)/$(InstallDir)" $(GnuMakeInstallExtra)
+$(install_base_documentation)
+for x in $(wildcard \
+	$(BuildDir)/AUTHORS* \
+	$(BuildDir)/ACKS* \
+	$(BuildDir)/CHANGES* \
+	$(BuildDir)/COPYING* \
+	$(BuildDir)/CREDITS* \
+	$(BuildDir)/NOTICE* \
+	$(BuildDir)/README* \
+	$(BuildDir)/INSTALL* \
+	$(BuildDir)/NEWS* \
+	$(BuildDir)/LICENSE* \
+	$(BuildDir)/ChangeLog*) ; do \
+	install -m 644 $$x $(InstallDir)/$(DocDir)/$(Name) ; \
+done
+rm -f $(InstallDir)/$(InfoDir)/dir
+rm -f $(InstallDir)/$(LibDir)/charset.alias
+rm -f $(InstallDir)/$(DataDir)/locale/locale.alias
+endef
+
+define test_inner_hook
+$(call test_universal)
+cd $(BuildDir) ; $(gnu_make) check || $(call error_color,One or more tests failed)
+endef
---
+++ Library/UnixFormula.mk
@@ -0,0 +1,16 @@
+# UnixFormula.mk - Standard build with make.
+# Copyright (c) 2011 Ruda Moura
+# Authors: Ruda Moura, Leonardo Santagada
+
+define build_inner_hook
+cd $(BuildDir) ; $(gnu_make) $(GnuMakeExtra) \
+	CFLAGS="$(CFlags)" CXXFLAGS="$(CxxFlags)" LDFLAGS="$(LdFlags)"
+endef
+
+define install_inner_hook
+$(install_base_documentation)
+endef
+
+define test_inner_hook
+$(test_universal)
+endef
